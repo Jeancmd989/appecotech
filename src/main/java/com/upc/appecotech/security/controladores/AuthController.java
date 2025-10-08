2c76dc6a36d937c326391d684a779aa308d061c2
Jean Paul Quispe Salvador <u202313101@upc.edu.pe>
2025-10-08 00:32:05 -0500

    Modificaciones

---
+++ src/main/java/com/upc/appecotech/security/controladores/AuthController.java
@@ -0,0 +1,57 @@
+package com.upc.appecotech.security.controladores;
+
+import com.upc.appecotech.security.dtos.AuthRequestDTO;
+import com.upc.appecotech.security.dtos.AuthResponseDTO;
+import com.upc.appecotech.security.servicios.CustomUserDetailsService;
+import com.upc.appecotech.security.util.JwtUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "${ip.frontend}", allowCredentials = "true", exposedHeaders = "Authorization")
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, CustomUserDetailsService userDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponseDTO> createAuthenticationToken(@RequestBody AuthRequestDTO authRequest) throws Exception {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getContraseña())
+        );
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getEmail());
+        final String token = jwtUtil.generateToken(userDetails);
+
+        Set<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toSet());
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Authorization", token);
+
+        AuthResponseDTO authResponseDTO = new AuthResponseDTO();
+        authResponseDTO.setRoles(roles);
+        authResponseDTO.setJwt(token);
+
+        return ResponseEntity.ok().headers(responseHeaders).body(authResponseDTO);
+    }
+}
