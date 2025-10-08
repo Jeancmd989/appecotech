a7333269bd8ddb8de6bdd4e750a031a5bbeef526
Jean Paul Quispe Salvador <u202313101@upc.edu.pe>
2025-10-08 00:33:24 -0500

    Modificaciones

--- src/main/java/com/upc/appecotech/interfaces/IUsuariorolIdService.java
+++
@@ -1,9 +0,0 @@
-package com.upc.appecotech.interfaces;
-
-import com.upc.appecotech.entidades.UsuariorolId;
-
-import java.util.List;
-
-public interface IUsuariorolIdService {
-    public List<UsuariorolId> listarUsuariorolIds();
-}
--- src/main/java/com/upc/appecotech/interfaces/IUsuariorolService.java
+++
@@ -1,9 +0,0 @@
-package com.upc.appecotech.interfaces;
-
-import com.upc.appecotech.entidades.Usuariorol;
-
-import java.util.List;
-
-public interface IUsuariorolService {
-    public List<Usuariorol> listarUsuariorols();
-}
